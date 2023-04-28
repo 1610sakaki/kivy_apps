ec8fc3027968228bed6cfd9b2989cdc23347226d
HirotoSakaki <skkcode1610@gmail.com>
2023-04-28 10:50:46 +0900

    不必要ファイルを削除 READMEついか

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 GUI application created with kivy
 
 Equipped with a calculator and screen transitions
+
+kivyで作成したGUIアプリケーション
+
+電卓と画面遷移をつけています
+
+## install.sh
+Ubuntuで、Pythonやビルドツール、音声・映像関連のライブラリをインストールするコマンドをまとめた。
+以下のパッケージがインストールされる
+
+- pip
+- C言語のコンパイラやmakeなどの開発ツールであるbuild-essential
+- Gitクライアント
+- Python2とPython3本体
+- Python開発に必要なライブラリ
+- 動画や音声の再生、変換に必要なライブラリであるffmpeg、gstreamerなど
+
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt-get install -y \
     python3-pip \
     build-essential \
@@ -17,8 +18,24 @@ sudo apt-get install -y \
     libavcodec-dev \
     zlib1g-dev
 
-# Install gstreamer for audio, video (optional)
+# 動画や音声の再生、変換に必要なライブラリ
 sudo apt-get install -y \
     libgstreamer1.0 \
     gstreamer1.0-plugins-base \
     gstreamer1.0-plugins-good
+
+# Kivyのインストール
+# 1. 仮想環境を作成する
+python3 -m venv venv
+
+# 2. 仮想環境をアクティブにする
+source venv/bin/activate
+
+# 3. pipを最新バージョンに更新する
+pip install --upgrade pip
+
+# 4. Kivyをインストールする
+pip install kivy
+
+# 5. インストールが完了したかどうかを確認する
+python -c "import kivy; kivy.require('1.11.1')"
